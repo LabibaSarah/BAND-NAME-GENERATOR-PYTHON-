a0ea2ebcb34c2f0c899d6f99eb887bb51eec951a
Labiba Mahmood <labibasarah67@gmail.com>
2025-02-27 11:12:55 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+name_of_the_city=input("What's the name of city you grew up in?\n")
+name_of_the_pet=input("Whats's the name of your pet?\n")
+print("Your band name could be "+name_of_the_city+" "+name_of_the_pet)
